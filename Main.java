2ad510b409f035399425289f276a11feffba862e
P-pPark <49361010+P-pPark@users.noreply.github.com>
2019-04-08 00:38:37 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+package com.company;
+import java.util.Scanner;
+    public class Main {
+
+
+
+        public static void main(String[] args) {
+// Local variable
+            // int swValue;
+// Display menu1
+            Menu M1 = new Menu();
+            M1.display1();
+            M1.option();
+
+
+
+//       Person A1 = PersonFactory.createPerson(People.DOCTOR);
+//       System.out.println (A1.display());
+
+
+
+        }
+    }
+
+//public static void main(String[] args) {
+
+//  Person sam = new Doctor("Sam", "therapeutic");
+
+//Person vic = new Patient("Vic", "flu", 14);
+
+
+
+//System.out.println(kate.toString());
+
+
+
+//  }
+//}
